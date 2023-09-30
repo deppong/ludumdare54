17090bbdc957e1c93ecb9c0bf41cff6712b0dfb7
NullCodeMusic <39009533+NullCodeMusic@users.noreply.github.com>
2023-09-30 16:52:40 -0700

    Heaalth bar

---
+++ objects/Healthbar.gd
@@ -0,0 +1,18 @@
+extends Control
+var value: int =3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$VBoxContainer/ProgressBar.set_value_no_signal(value)
+	
+func change_value(amount):
+	value = value + amount
+	
+func get_value():
+	return value
+	
