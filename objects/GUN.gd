f6bb2b7a03fe45e95c5924f8dc44c3e84422471e
Ghastlymicrowave <supervaughanbros@gmail.com>
2023-10-01 19:55:26 -0400

    fix player arms

--- objects/GUN.gd
+++ objects/GUN.gd
@@ -1,8 +1,9 @@
 extends Sprite2D
 
-
+var startingPos: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	startingPos = position
 	pass # Replace with function body.
 
 
@@ -17,3 +18,12 @@ func _process(delta):
 		z_index=0
 	else:
 		z_index=1
+	var dir = (get_global_mouse_position() - global_position).normalized()
+	var dot = dir.dot(Vector2(0,1))
+	if dot > 0:
+		var offset = Vector2(17,-35)
+		if (get_global_mouse_position().x>get_parent().position.x):
+			offset *= Vector2(-1,1)
+		position = startingPos + (offset * dot)
+	else:
+		position = startingPos
