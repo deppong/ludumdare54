2399a4a3abaa458bb02ff862d4709bdbbbee9d02
Isaiah Deppong <isaiahjdeppong@gmail.com>
2023-09-30 20:06:35 -0400

    added healthbar to main, added an enemy cap and random time to spawn them up to the max amount

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,17 +1,21 @@
 extends Node
 
 @export var enemy_tscn: PackedScene
-var enemy_count
+var enemy_count = 0
 
+func _process(_delta):
+	$spawn_timer.wait_time = randf_range(1.0, 5.0)
 
 func _on_spawn_timer_timeout():
-	var enemy = enemy_tscn.instantiate()
-	var spawn_location = $spawn_path/spawn_point
-	spawn_location.progress_ratio = randf()
-	
-	var direction = (spawn_location.rotation + PI /2)
-	
-	enemy.position = spawn_location.position
-	
-	get_parent().add_child(enemy)
-	
+	if (enemy_count < 5):
+		var enemy = enemy_tscn.instantiate()
+		var spawn_location = $spawn_path/spawn_point
+		spawn_location.progress_ratio = randf()
+		
+		var direction = (spawn_location.rotation + PI /2)
+		
+		enemy.position = spawn_location.position
+		
+		get_parent().add_child(enemy)
+		enemy_count+=1
+		
